8acd0971fc84879910a0d0e45c1e773803479908
lpelypenko <2556875+lpelypenko@users.noreply.github.com>
2024-10-16 15:36:01 +0900

    Finished 30

--- src/main/java/com/c/refactoring/movie/Movie.java
+++ src/main/java/com/c/refactoring/movie/Movie.java
@@ -2,10 +2,18 @@ package com.c.refactoring.movie;
 
 import com.c.refactoring.StringUtils;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Movie {
 
     String rating;
-
+    private static final Set<String> VALID_B_RATING = new HashSet<String>() {{
+        add("B1");
+        add("B2");
+        add("B3");
+        add("B4");
+    }};
     public Movie(String rating) {
         super();
         this.rating = rating;
@@ -19,21 +27,21 @@ public class Movie {
     Where x represents any digit between 0 and 9, and y represents 
     any digit between 1 and 4*/
     public boolean isValidRating() {
-        if (this.getRating() != null) {
-            if (this.getRating().substring(0, 1).equalsIgnoreCase("B")
-                    && this.getRating().length() == 2) {
-                if (StringUtils.isNumeric(this.getRating().substring(1, 2))
-                        && Integer.parseInt(this.getRating().substring(1, 2)) > 0
-                        && Integer.parseInt(this.getRating().substring(1, 2)) < 5)
-                    return true;
-
-            } else if (this.getRating().substring(0, 1).equalsIgnoreCase("A")
-                    && this.getRating().length() == 3
-                    && StringUtils.isNumeric(this.getRating().substring(1, 3)))
-                return true;
-
+        if (getRating() == null) {
+            return false;
         }
-        return false;
+        return isValidBRating() || isValidARating();
+    }
+
+    private boolean isValidBRating() {
+        return VALID_B_RATING.contains(getRating());
+    }
+
+    private boolean isValidARating() {
+        String rating = this.getRating();
+        String firstCharacter = rating.substring(0, 1);
+        return firstCharacter.equalsIgnoreCase("A")
+                && rating.length() == 3 && StringUtils.isNumeric(rating.substring(1, 3));
     }
 
     public void setRating(String rating) {
